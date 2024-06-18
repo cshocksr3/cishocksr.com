d87342e991145d499b5adef8c0f43d78284d2107
Christopher Shockley <cishockleysr@gmail.com>
2024-06-18 11:16:44 -0400

    new site

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Chris Shockley | Software Engineer",
+  url: "https://cishocksr.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Chris Shockley",
+  links: {
+    twitter: "https://x.com/cshocksr",
+    github: "https://github.com/cshocksr3",
+    personalSite: "https://jamesshopland.com",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
